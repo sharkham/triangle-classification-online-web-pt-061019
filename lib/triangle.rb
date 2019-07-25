48f79f491367e5ddf118b186b662b9c918b761c8
Sam Markham <sam.e.markham@gmail.com>
2019-07-25 20:54:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,19 +6,24 @@ class Triangle
     @c = c
   end
 
-  def kind
+  def real_triangle?
     if self.a <= 0 || self.b <= 0 || self.c <= 0
       raise TriangleError
     elsif self.a + self.b <= self.c || self.a + self.c <= self.b || self.b + self.c <= self.a
       raise TriangleError
     else
-      if self.a == self.b && self.b == self.c
-        :equilateral
-      elsif self.a == self.b || self.a == self.c || self.b == self.c
-        :isosceles
-      else
-        :scalene
-      end
+      true
+    end
+  end
+
+  def kind
+    real_triangle?
+    if self.a == self.b && self.b == self.c
+      :equilateral
+    elsif self.a == self.b || self.a == self.c || self.b == self.c
+      :isosceles
+    else
+      :scalene
     end
   end
 
